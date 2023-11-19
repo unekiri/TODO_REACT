5f22846c18d9224579f3cd9015af08e783b6d72f
nakaoshougo <knpy0508@gmail.com>
2023-11-19 15:57:05 +0900

    POSTアクションを実装できました

---
+++ todo_react/src/components/Addtext.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Header} from './Header';
+import { addItem } from './PostItem';
+import '../stylesheets/style.css';
+
+export const Addtext = () => {
+  // useFormの初期化
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const handleOnSubmit = (values) => {
+    console.log(values);
+    addItem();
+  }
+
+  return (
+    <>
+    <Header />
+    <main>
+    <div className="container">
+      <form onSubmit={handleSubmit(handleOnSubmit)}>
+        <div className="incomplete-area">
+          <div className="another-page">
+            <p>タスクの内容</p>
+            <textarea 
+              id="add-name" placeholder="100文字以内の入力"
+              {...register('name', {
+                 required: '入力必須です。', 
+                 maxLength : {value: 100, message: '100文字以内で入力して下さい。'}
+              })}
+            />
+            {errors.name && <span className="error-message">{errors.name.message}</span>}
+          </div>
+          <div className="another-page">
+            <p>完了予定日</p>
+            <input 
+              id="add-date" type="date" 
+              {...register('date', { 
+                  required: '入力必須です。' }
+              )}
+            />
+            {errors.date && <span className="error-message">{errors.date.message}</span>}
+          </div>
+          <div className="end_button">
+            <button type="submit">送信</button>
+          </div>
+        </div>
+      </form>
+    </div>
+    </main>
+    </>
+  );
+};
---
+++ todo_react/src/components/PostItem.js
@@ -0,0 +1,35 @@
+// POSTアクションのイベントハンドラ
+
+export const addItem = () => {
+  const addNameTextbox = document.getElementById('add-name');
+  const addDate = document.getElementById('add-date');
+
+  // Dateオブジェクトを生成し、そのDateオブジェクトをUTCに変更する
+  const UTCDate = new Date(addDate.value);
+  UTCDate.setUTCHours(0, 0, 0, 0);
+
+  console.log(UTCDate);
+
+  const item = {
+    name: addNameTextbox.value,
+    isComplete: false,
+    date: UTCDate.toISOString()
+  };
+
+  fetch("https://localhost:7034/api/todoitems", {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    // JSON文字列への変換
+    body: JSON.stringify(item)
+  })
+    // リクエストが成功すると、下記でJSONオブジェクトを処理する
+    .then(response => response.json())
+    .then(() => {
+      // HOMEへのリダイレクト
+      window.location.href = 'index.html';
+    })
+    .catch(error => console.error('Unable to add item.', error));
+}
