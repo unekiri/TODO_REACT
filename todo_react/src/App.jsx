a8ba7f9a40f7192e805e567904f178e3a59abc7f
nakaoshougo <knpy0508@gmail.com>
2023-11-19 10:39:31 +0900

    add_textページに遷移できようにコードを修正しました

--- todo_react/src/App.jsx
+++ todo_react/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home }  from './components/Home';
 import { Incomplete } from './components/Incomplete';
 import { Complete } from './components/Complete';
+import { Add_text } from './components/Add_text';
+
 import './stylesheets/style.css';
 
 export const  App = () => {
@@ -13,6 +15,7 @@ export const  App = () => {
           <Route exact path="/" element={<Home />} />
           <Route path="/incomplete" element={<Incomplete />} />
           <Route path="/complete" element={<Complete />} />
+          <Route path="/add_text" element={<Add_text />} />
         </Routes> 
       </BrowserRouter>
     </>
