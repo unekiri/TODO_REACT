e789a00efe858d3c6af0b7517effa2b53af74f82
nakaoshougo <knpy0508@gmail.com>
2023-11-20 16:19:33 +0900

    完了ページに登録したTODOが表示されるようにコードを修正しました

--- todo_react/src/components/Complete.jsx
+++ todo_react/src/components/Complete.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Header} from './Header';
+import { getItems } from './GetItems'; 
 import '../stylesheets/style.css';
 
 export const Complete = () => {
+  useEffect(() => {
+    getItems(true);
+  }, []);
   return (
     <>
     <Header />
