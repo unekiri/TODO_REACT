c707a6ce22b3b29f7e869b542700ccf4586abbf7
nakaoshougo <knpy0508@gmail.com>
2023-12-12 23:15:09 +0900

    editアクションのイベントハンドラを実装中

--- todo_react/src/App.jsx
+++ todo_react/src/App.jsx
@@ -4,6 +4,8 @@ import { Home }  from './components/Home';
 import { Incomplete } from './components/Incomplete';
 import { Complete } from './components/Complete';
 import { Addtext } from './components/Addtext';
+import { Incomplete_Edit } from './components/Incomplete_Edit';
+import { Complete_Edit } from './components/Complete_Edit'
 
 import './stylesheets/style.css';
 
@@ -15,6 +17,8 @@ export const  App = () => {
         <Route path="/incomplete" element={<Incomplete />} />
         <Route path="/complete" element={<Complete />} />
         <Route path="/addtext" element={<Addtext />} />
+        <Route path="/incomplete_edit" element={<Incomplete_Edit />} />
+        <Route path="/complete_edit" element={<Complete_Edit />} />
       </Routes> 
     </BrowserRouter>
   );
