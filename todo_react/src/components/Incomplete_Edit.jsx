770229d0ff9c868eab5080d88dd77192a41f3659
nakaoshougo <knpy0508@gmail.com>
2023-12-20 22:38:47 +0900

    編集フォームの初期値を表示できるようにコードを修正しました(ただ、共通化がうまくできていない)

--- todo_react/src/components/Incomplete_Edit.jsx
+++ todo_react/src/components/Incomplete_Edit.jsx
@@ -3,11 +3,15 @@ import { useForm } from 'react-hook-form';
 import { Header } from './Header';
 import { Load } from './Load';
 import { edit_updateItem } from './Edit_UpdateItem';
-import { Form } from './Form';
 import '../stylesheets/style.css';
 
 export const Incomplete_Edit = () => {
-  const { setValue } = useForm();
+  const { register, handleSubmit, formState: { errors }, setValue } = useForm({
+    defaultValues: {
+      name: '',
+      date: '',
+    },
+  });
 
   useEffect(() => {
     Load((httpData) => {
@@ -16,10 +20,10 @@ export const Incomplete_Edit = () => {
       // 元のTODO内容をフォームに表示
       setValue('name', httpData.name);
       // 元の日付をフォームに表示
-      setValue('date', formattedDate.split("/").join("-"));
+      setValue('date', formattedDate);
     });
   }, [setValue]);
-
+  
   const handleOnSubmit = () => {
     edit_updateItem(false);
   }
@@ -29,7 +33,34 @@ export const Incomplete_Edit = () => {
     <Header />
     <main>
       <div className="container">
-        <Form onSubmit={handleOnSubmit} />
+        <form onSubmit={handleSubmit(handleOnSubmit)}>
+          <div className="incomplete-area">
+            <div className="another-page">
+              <p>タスクの内容</p>
+              <textarea 
+                id="name" placeholder="100文字以内の入力"
+                {...register('name', {
+                  required: '入力必須です。', 
+                  maxLength : {value: 100, message: '100文字以内で入力して下さい。'}
+                })}
+              />
+              {errors.name && <span className="error-message">{errors.name.message}</span>}
+            </div>
+            <div className="another-page">
+              <p>完了予定日</p>
+              <input 
+                id="date" type="date" 
+                {...register('date', { 
+                    required: '入力必須です。' }
+                )}
+              />
+              {errors.date && <span className="error-message">{errors.date.message}</span>}
+            </div>
+            <div className="end_button">
+              <button type="submit">送信</button>
+            </div>
+          </div>
+        </form>
       </div>
     </main>
     </>
