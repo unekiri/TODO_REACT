21b8d0fdca4f9d91d427dd538fb87abf73c02f63
nakaoshougo <knpy0508@gmail.com>
2023-12-21 23:16:35 +0900

    完了したTODOの編集ページを作成しました

---
+++ todo_react/src/components/Complete_Edit.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { Header } from './Header';
+import { Load } from './Load';
+import { edit_updateItem } from './Edit_UpdateItem';
+import '../stylesheets/style.css';
+
+export const Complete_Edit = () => {
+  const { register, handleSubmit, formState: { errors }, setValue } = useForm({
+    defaultValues: {
+      name: '',
+      date: '',
+    },
+  });
+
+  useEffect(() => {
+    Load((httpData) => {
+      const date = new Date(httpData.date);
+      const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+      // 元のTODO内容をフォームに表示
+      setValue('name', httpData.name);
+      // 元の日付をフォームに表示
+      setValue('date', formattedDate);
+    });
+  }, [setValue]);
+  
+  const handleOnSubmit = () => {
+    edit_updateItem(true);
+  }
+
+  return (
+    <>
+    <Header />
+    <main>
+      <div className="container">
+        <form onSubmit={handleSubmit(handleOnSubmit)}>
+          <div className="complete-area">
+            <div className="another-page">
+              <p>タスクの内容</p>
+              <textarea 
+                id="name" placeholder="100文字以内の入力"
+                {...register('name', {
+                  required: '入力必須です。', 
+                  maxLength : {value: 100, message: '100文字以内で入力して下さい。'}
+                })}
+              />
+              {errors.name && <span className="error-message">{errors.name.message}</span>}
+            </div>
+            <div className="another-page">
+              <p>完了予定日</p>
+              <input 
+                id="date" type="date" 
+                {...register('date', { 
+                    required: '入力必須です。' }
+                )}
+              />
+              {errors.date && <span className="error-message">{errors.date.message}</span>}
+            </div>
+            <div className="end_button">
+              <button type="submit">送信</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </main>
+    </>
+  );
+};
