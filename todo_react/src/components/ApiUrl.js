9618631a3b043c80b9ec0939f9fe2bf826749902
nakaoshougo <knpy0508@gmail.com>
2024-01-25 23:05:13 +0900

    uriを修正しました(+実行環境でエラーが出る箇所のメモ)

--- todo_react/src/components/ApiUrl.js
+++ todo_react/src/components/ApiUrl.js
@@ -1,2 +1,4 @@
 // WebAPIのURI
-export const uri = 'https://nakaoback.azurewebsites.net/';
+// 実行環境において、idを取得する関数がうまく機能していないみたい
+
+export const uri = 'https://nakaoback.azurewebsites.net/api/todoitems';
